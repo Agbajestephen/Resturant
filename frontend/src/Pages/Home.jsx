baeb8bed706a4aa90950d26e769161e5a4eff4c1
Agbajestephen <agbajestephen5@gmail.com>
2026-02-02 15:34:37 +0100

    Create Home.jsx

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import HeroSection from "../components/HeroSection.jsx";
+import About from "../components/About.jsx";
+import Qualities from "../components/Qualities.jsx";
+import Menu from "../components/Menu.jsx";
+import WhoAreWe from "../components/WhoAreWe.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <About />
+      <Qualities />
+      <Menu />
+      <WhoAreWe/>
+    </>
+  );
+};
+
+export default Home;

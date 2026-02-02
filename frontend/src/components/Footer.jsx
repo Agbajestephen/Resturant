0cbdcde055e8bb6884d6f9ec7c76c33e1aa44115
Agbajestephen <agbajestephen5@gmail.com>
2026-02-02 20:46:25 +0100

    Create Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">GOKE</div>
+          <div className="right">
+            <p>1, Obanta Apapa Lagos, Nigeria</p>
+            <p>Open: 09:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By GOKETHEDEV</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By GokeTheDev.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

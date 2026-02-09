06f5e1c57b54b2642639736852c229b2a5ce80c5
Agbajestephen <agbajestephen5@gmail.com>
2026-02-09 06:34:03 +0100

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -26,7 +26,6 @@ const Reservation = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          withCredentials: true,
         }
       );
       toast.success(data.message);
